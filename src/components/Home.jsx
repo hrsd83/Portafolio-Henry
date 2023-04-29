24d1ce8d7e46e82ce628ef734c954ce0b04637ad
hrsd83 <hrsd83@gmail.com>
2023-04-28 21:15:58 -0300

    Se crea el archivo de experiencia en tecnologias

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,9 @@ import {MdKeyboardArrowRight} from 'react-icons/md'
 const Home = () => {
   return (
     <div className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-300'>
+
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-            <div className='flex flex-col justify-center h-full'>
+            <div className='flex flex-col justify-center h-full sm: mb-40'>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>Soy Tester QA</h2>
 
                 <p className='text-gray-500 py-4'>Soy una persona proactiva, excelente compañero de equipo, organizado, responsable y me gusta ayudar a los demas. Comenzar mi carrera profesional en una empresa reconocida me motiva arduamente,  tengo gran facilidad de aprender procesos nuevos y un inmenso placer en integrarme al equipo de trabajo, por lo que le puedo asegurar  que en breve  estaré desempeñando las funciones del puesto optimamente.</p>
