f545020777afdec2f1babd85540e46477270a903
hrsd83 <hrsd83@gmail.com>
2023-04-28 21:30:25 -0300

    Se modifica el archivo NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,14 +26,14 @@ const NavBar = () => {
 
     ]
   return (
-    <div className='flex justify-between items-center w-full h-20 text-white fixed bg-black px-4'>
+    <div className='flex justify-between items-center w-full h-20 text-white bg-black px-4 fixed'>
         <div>
             <h1 className='text-5xl font-signature ml-2'>HenryS</h1>
         </div>
         <ul className='hidden md:flex'>
         {/* Se mapean los links */}
         {links.map(({ id,link }) =>(
-            <li key={ id } className='px-4 cursor-pointer capitalize font- text-gray-400 hover:scale-110 duration-200 '>
+            <li key={ id } className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200 '>
                 { link }
             </li>
         ))}    
