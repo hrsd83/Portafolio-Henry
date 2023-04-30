1635584b788a2fac2345b3c6b83cad4128a2e652
hrsd83 <hrsd83@gmail.com>
2023-04-30 18:15:35 -0300

    Se crea link para ir a la app de clima y se arregla titulo del cv pdf

--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -8,7 +8,8 @@ const Portafolio = () => {
 
         {
             id: 1,
-            src: Clima2
+            src: Clima2,
+            url: 'https://clima-five.vercel.app/',
         },
     ]
   return (
@@ -16,15 +17,16 @@ const Portafolio = () => {
         <div className='max-w-screen-xl p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portafolio</p>
-                <p className='py-6'>Mira algunos de mis trabajos aqui</p>
+                <p className='py-6'>Mira algunos trabajos a cual se le realizo pruebas</p>
             </div>
                 
             <div className=' grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-30 sm:px-0'
             >
-                {portafolios.map(({id, src}) => ( 
+                {portafolios.map(({id, src,url}) => ( 
                     <div key={id} className=' shadow-md shadow-gray-500 rounded-lg '>
-                    <img src= {src} alt='clima' className='rounded-md duration-200 hover:scale-105'></img>
+                    <a href={url}> <img src= {src} alt='' className='rounded-md duration-200 hover:scale-105'></img></a>
                     <div className='flex items-center justify-center'>
+                    
 
                         <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>
                         <a href='/public/Clima/prueba-de-texto.side' download>Selenium</a>
