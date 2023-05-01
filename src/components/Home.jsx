2c3ba69ee8ca5cb0c19c85e52d6ea5a3e4b53559
hrsd83 <hrsd83@gmail.com>
2023-05-01 11:08:43 -0300

    Se modifica el button de descarga de PDF

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <div name = "Home" className=  'h-screen w-full bg-gradient-to-b from-black to-gray-800 '>
 
         <div className='max-w-screen-lg mx-auto  flex flex-col items-center justify-center h-full px-4 md:flex-row  '>
-            <div className='flex flex-col justify-center h-full   '>
+            <div className='flex flex-col justify-center h-full mt-5'>
                 <h2 className='text-4xl sm:text-7xl font-bold  text-white'>Tester QA</h2>
 
                 <p className='text-gray-500 py-4 hidden sm:block'>Soy una persona proactiva, excelente compañero de equipo, organizado, responsable y me gusta ayudar a los demas. Comenzar mi carrera profesional en una empresa reconocida me motiva arduamente,  tengo gran facilidad de aprender procesos nuevos y un inmenso placer en integrarme al equipo de trabajo, por lo que le puedo asegurar  que en breve  estaré desempeñando las funciones del puesto optimamente.</p>
