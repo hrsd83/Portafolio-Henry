21ea49d1de37428cd97d93dd02041a6591218d18
hrsd83 <hrsd83@gmail.com>
2023-05-01 07:43:50 -0300

    Se arregla desboradamiento de formulario e imagen de perfil

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Contacto = () => {
   return (
-    <div name= "Contacto" className=' w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white'>
+    <div name= "Contacto" className='bg-gradient-to-b from-black to-gray-800 p-4 text-white max-w-full w-full h-full'>
       <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full '>
         <div className='pb-8 '>
             <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Formulario</p>
             <p className='py-6 '>Llene el formulario para contactarme</p>
         </div>
-        <div className='flex justify-center items-center'>
+        <div className='flex justify-center items-center<'>
             <form action='https://getform.io/f/69c47dbc-f431-458d-b5a6-1146fbbe7a32' method='POST' className='flex flex-col w-full md:w-1/2'> 
                 <input  
                 type='text' 
@@ -23,7 +23,7 @@ const Contacto = () => {
                 placeholder='Ingrese su email'
                 className='p-2 my-4 bg-gradient-to-b from-black to-gray-800 rounded-md text-white focus: outline-none'>
                 </input>
-                <textarea name='message' rows="10" placeholder='Ingresa un mensaje' className='p-2 bg-gradient-to-b from-black to-gray-800 rounded-md text-white focus:outline-none'  >
+                <textarea name='message' rows="10" placeholder='Ingresa un mensaje' className='p-2 bg-gradient-to-b from-black to-gray-800 rounded-md text-white focus:outline-none max-w-full w-full h-full'  >
                 
 
                 </textarea>
